0b545adde560f45199fe851cde7677e3be39a27e
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-03-16 15:37:43 -0700

    Game component

--- src/index.js
+++ src/index.js
@@ -40,3 +40,25 @@ class Board extends React.component {
         );
     }
 }
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+//   ===============================================
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
