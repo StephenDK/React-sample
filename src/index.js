34045d7008a045d48a7c74450a3abf2f8de7d2b7
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-03-15 17:02:12 -0700

    reactDOM.render

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(
+    <nav>
+        <h3>Welcome to React </h3>
+    </nav>,
+    document.getElementById('root')
+);
+
+ReactDOM.render(
+    <main>
+        <div>
+            <p>My name is Stephen and this is a React application </p>
+        </div>
+    </main>,
+    document.getElementById('second')
+);
